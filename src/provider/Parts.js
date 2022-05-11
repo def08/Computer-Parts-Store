b4442a545ef319ff6bddd154faa5a1040be84c30
Your <denilsonfrancis.2002@gmail.com>
2022-05-11 11:35:11 -0400

    Fixed images

--- src/provider/Parts.js
+++ src/provider/Parts.js
@@ -1,24 +1,24 @@
 export const Parts = [
     {
-        Image: <img src="keycaps.jpg"  alt="Textured Keycaps for mechanical keyboard" width="200" height="300"/>,
+        Image: require("./keycaps.jpg"),
         PartName: 'Textured Keyboard KeyCaps',
         Price: '15',
         Quantity: '13',
         id: '1'
     },{
-        Image: <img src="bfemaleport.jpg"  alt="USB Type B Female Port" width="200" height="300"/>,
+        Image: require("./bfemaleport.jpg"),
         PartName: 'Female USB B Port',
         Price: '15',
         Quantity: '5',
         id: '2'
     },{
-        Image: <img src="cfemaleport.jpg"  alt="USB Type C Female Port" width="200" height="300"/>,
+        Image: require("./cfemaleport.jpg"),
         PartName: 'USB C Port',
         Price: '30',
         Quantity: '10',
         id: '3'
     },{
-        Image: <img src="lightstrips.jpg"  alt="LED Light Strips" width="200" height="300"/>,
+        Image: require("./lightstrips.jpg"),
         PartName: 'LED light strips',
         Price: '20',
         Quantity: '2',
